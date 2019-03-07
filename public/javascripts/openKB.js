fcb7c5fdcad92b4f2096933dfa6c84c0fb1a007b
Dominic Crousset <dcrousset@cognito-app.com>
2019-03-07 17:14:32 -0500

    Redmine #1410 - le areYouSure.js était sur tous les forms, incluant la recherche, doh !, est maintenant que sur les <form> d'édition

--- public/javascripts/openKB.js
+++ public/javascripts/openKB.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $(function() {
-        $('form').areYouSure(
+        $('#edit_form').areYouSure(
             {
                 message: 'It looks like you have been editing something. '
                 + 'If you leave before saving, your changes will be lost.'
